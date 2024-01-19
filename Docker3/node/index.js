abeb9fe6f65b5ec0685ba087de3b3801f692c3e3
PritOnce <ngojlia2@nigul.cide.es>
2024-01-19 17:37:35 +0100

    docker arquitectura dos capas

---
+++ Docker3/node/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+var mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+});
+
+connection.connect();
+
+app.get("/", (req, res) => {
+    res.send("HOLA MUNDO");
+});
+
+app.get("/bd", (req, res) => {
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+    res.json(rows[0].solution);
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+// connection.end();
